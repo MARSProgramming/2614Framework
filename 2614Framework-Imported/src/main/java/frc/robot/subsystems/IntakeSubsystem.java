9ca142faec2e464009de3f87745ce12105465bbc
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2023-01-21 11:12:04 -0500

    fixed motor names

--- 2614Framework-Imported/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ 2614Framework-Imported/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,21 +1,27 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
     
-    private TalonFX Motor0 = new TalonFX(0);
-    private TalonFX Motor13 = new TalonFX(13);
+    private TalonSRX Motor0 = new TalonSRX(0);
+    private TalonSRX Motor13 = new TalonSRX(13);
     
     public IntakeSubsystem() {
-
+        Motor0.setNeutralMode(NeutralMode.Brake);
+        Motor13.setNeutralMode(NeutralMode.Brake);
+        Motor13.follow(Motor13);
+        Motor0.setInverted(true);
+        Motor0.setInverted(true);
     }
 
     public void RunMotors(double voltage) {
         Motor0.set(ControlMode.PercentOutput, voltage);
         Motor13.set(ControlMode.PercentOutput, voltage);
+        
     }
 }
