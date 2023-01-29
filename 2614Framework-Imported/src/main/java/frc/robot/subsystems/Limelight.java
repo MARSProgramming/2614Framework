4ae5188540f854d81f5827a80ed48c65ced84f5a
Epicr12 <57331205+Epicr12@users.noreply.github.com>
2023-01-29 14:18:39 -0500

    Update Limelight.java

--- 2614Framework-Imported/src/main/java/frc/robot/subsystems/Limelight.java
+++ 2614Framework-Imported/src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,29 +1,46 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 
 public class Limelight extends SubsystemBase{
+
     private NetworkTable table; 
     
     public Limelight(){ 
         table = NetworkTableInstance.getDefault().getTable("limelight"); 
     }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    SmartDashboard.putNumberArray("camerapose_targetspace", getCameraPoseTargetSpace());
+    SmartDashboard.putNumberArray("targetpose_cameraspace", getTargetPoseCameraSpace());
+    SmartDashboard.putNumberArray("tergetpose_robotspace", getTargetPoseRobotSpace()); 
+    SmartDashboard.putNumberArray("botpose_targetspace", getRobotPoseTargetSpace());
+    SmartDashboard.putNumber("tid", getTargetID()); 
+    SmartDashboard.putNumberArray("botpose", getBotPose()); 
+    SmartDashboard.putNumberArray("BotPoseRed", getBotPoseRed()); 
+    SmartDashboard.putNumberArray("botposeblue", getBotPoseBlue()); 
+
+    }
+  
     
     public double[] getCameraPoseTargetSpace(){
         return table.getEntry("camerapose_targetspace").getDoubleArray(new double[6]);
     }
 
     public double[] getTargetPoseCameraSpace(){
-        return table.getEntry("targetpose_cameraspace").getDoubleArray(new double[6]);
+        return table.getEntry("targetpose_cameraspace").getDou  bleArray(new double[6]);
     }
 
     public double[] getTargetPoseRobotSpace(){
@@ -34,8 +51,8 @@ public class Limelight extends SubsystemBase{
         return table.getEntry("botpose_targetspace").getDoubleArray(new double[6]); 
     }
 
-    public NetworkTableEntry getTargetID(){
-        return table.getEntry("tid");  
+    public double getTargetID(){
+        return table.getEntry("tid").getDouble(0);  
     }
 
     public double[] getBotPose(){ 
@@ -49,4 +66,6 @@ public class Limelight extends SubsystemBase{
     public double[] getBotPoseBlue(){ 
         return table.getEntry("botpose_wpiblue").getDoubleArray(new double[6]); 
     }
+
+    
 }
